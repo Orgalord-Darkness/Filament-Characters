2ec2330ea95df13f8ff8b58f3be633b926b3c6e3
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-01-24 10:30:46 +0100

    Création de base de données filament + composer require css debugbar ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,10 +16,12 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
+     * @return void 
      * Bootstrap any application services.
      */
     public function boot(): void
     {
         //
+        Schema::defaultStringLength(191); 
     }
 }
