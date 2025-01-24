15845fa09c8ef9970c15279ef3cc3f5025b9f079
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-01-24 11:55:24 +0100

    Création entité Character et table characters

---
+++ app/Models/Character.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Character extends Model
+{
+    protected $fillable = 
+    [
+        'firstname', 
+        'lastname', 
+        'description', 
+        'power', 
+        'role',
+    ]; 
+}
